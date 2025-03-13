03368598fe58e9a3077b512ade026f069246ef05
Yodapythonpp <thejasshetty04@gmail.com>
2025-03-13 18:40:42 +0530

    Second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Whats going on, here is a change i've made\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
